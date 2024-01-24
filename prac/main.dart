fadf03591575ab954534d8a755a805547d85109f
Billy272 <billylaroi@gmail.com>
2024-01-24 10:21:53 +0300

    commit

--- prac/main.dart
+++ prac/main.dart
@@ -1,5 +1,5 @@
 void main() {
-  User user1 = User();
+  User user1 = User("No Friend", 21);
   print(user1.userName);
   print(user1.age);
   user1.login();
@@ -8,6 +8,12 @@ void main() {
 class User {
   String userName = 'Billy';
   int age = 21;
+
+  //use of a constructor in dart
+  User(String userName, int age) {
+    this.userName = userName;
+    this.age = age;
+  }
 
   void login() {
     print("Logged in!");
