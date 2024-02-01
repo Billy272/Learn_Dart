cbffb5733da9b632ba7b87346ae879ddb7c7ddd1
Billy272 <billylaroi@gmail.com>
2024-02-01 16:20:07 +0300

    commit

--- basic_page/lib/main.dart
+++ basic_page/lib/main.dart
@@ -2,32 +2,42 @@ import 'package:flutter/material.dart';
 
 // ignore: prefer_const_constructors
 void main() => runApp(
-      MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text('First App'),
-            centerTitle: true,
-            backgroundColor: const Color.fromARGB(255, 5, 255, 218),
-          ),
-          // ignore: prefer_const_constructors
-          body: Center(
-            child: const Text(
-              'Hello World!!',
-              style: TextStyle(
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 2.0,
-                  color: Colors.blue),
-            ),
-          ),
-          // ignore: prefer_const_constructors
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {},
-            backgroundColor: const Color.fromARGB(255, 5, 255, 218),
-            // ignore: prefer_const_constructors
-            child: Text('Click'),
+      const MaterialApp(
+        home: Home(),
+      ),
+    );
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First App'),
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 5, 255, 218),
+      ),
+      // ignore: prefer_const_constructors
+      body: Center(
+        child: const Text(
+          'Hello World!!',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.blue,
+            fontFamily: 'IndieFlower',
           ),
         ),
       ),
+      // ignore: prefer_const_constructors
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: const Color.fromARGB(255, 5, 255, 218),
+        // ignore: prefer_const_constructors
+        child: Text('Click'),
+      ),
     );
+  }
+}
