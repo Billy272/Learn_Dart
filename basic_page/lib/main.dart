d0da57794e3500b5cd7cb16f40e00aa0d3abab42
Billy272 <billylaroi@gmail.com>
2024-02-01 12:02:39 +0300

    commit

--- basic_page/lib/main.dart
+++ basic_page/lib/main.dart
@@ -4,10 +4,11 @@ import 'package:flutter/material.dart';
 void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('First App'),
-          centerTitle: true,
-        ),
-        body: const Text('Hello World'),
-      ),
+          appBar: AppBar(
+            title: const Text('First App'),
+            centerTitle: true,
+          ),
+          body: const Center(
+            child: Text('Hello World'),
+          )),
     ));
