d1f6c4b87e6ba1fc01d0227036c870e5ae45013e
Billy272 <billylaroi@gmail.com>
2024-02-05 11:38:05 +0300

    commit

--- dah_app/lib/main.dart
+++ dah_app/lib/main.dart
@@ -13,8 +13,21 @@ class Dahapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 4, 158, 235),
       appBar: AppBar(
         title: const Text('Dah Great App'),
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 6, 251, 255),
+        elevation: 0.0,
+      ),
+      body: const Padding(
+        padding: EdgeInsets.fromLTRB(30, 20, 30, 0),
+        child: Column(
+          children: <Widget>[
+            Text('Hello, Dah!',
+                style: TextStyle(fontSize: 20, color: Colors.white)),
+          ],
+        ),
       ),
     );
   }
