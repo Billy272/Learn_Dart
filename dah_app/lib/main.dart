e2871754ebf9bc882aac07900d778bb783ba636d
Billy272 <billylaroi@gmail.com>
2024-03-09 09:29:48 +0300

    Refactor main.dart to use bottom navigation bar

--- dah_app/lib/main.dart
+++ dah_app/lib/main.dart
@@ -1,108 +1,121 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  // ignore: prefer_const_constructors
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
-    // ignore: prefer_const_constructors
-    home: Dahapp(),
+    home: Rawyalty(),
   ));
 }
 
-class Dahapp extends StatefulWidget {
-  const Dahapp({Key? key}) : super(key: key);
+class Rawyalty extends StatelessWidget {
+  const Rawyalty({super.key});
 
   @override
-  // ignore: library_private_types_in_public_api
-  _DahappState createState() => _DahappState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Rawyalty App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const MyHomePage(),
+    );
+  }
 }
 
-class _DahappState extends State<Dahapp> {
-  int population = 1000000;
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+  @override
+  // ignore: library_private_types_in_public_api
+  _MyHomePageState createState() => _MyHomePageState();
+}
 
-  void _incPopulation() {
+class _MyHomePageState extends State<MyHomePage> {
+  int _currentIndex = 0;
+
+  final List<Widget> _children = [
+    const HomeScreen(),
+    const ServicesScreen(),
+    const AboutScreen(),
+    const AccountScreen(),
+  ];
+
+  void onTabTapped(int index) {
     setState(() {
-      population += 12000;
+      _currentIndex = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 4, 158, 235),
       appBar: AppBar(
-        title: const Text('Dah Great City'),
-        centerTitle: true,
-        backgroundColor: const Color.fromARGB(255, 6, 251, 255),
-        elevation: 0.0,
+        title: const Text('Bottom Navigation App'),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incPopulation,
-        backgroundColor: const Color.fromARGB(255, 226, 255, 5),
-        child: const Icon(Icons.add),
+      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+      body: _children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: onTabTapped,
+        currentIndex: _currentIndex,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Services',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.info),
+            label: 'About',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: 'Account',
+          ),
+        ],
       ),
-      body: const Padding(
-        padding: EdgeInsets.fromLTRB(30, 20, 30, 0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/future_city.jpg'),
-                radius: 100,
-              ),
-            ),
-            Divider(
-              height: 80,
-              color: Colors.yellowAccent,
-            ),
-            Text('CITY:',
-                style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white)),
-            SizedBox(height: 10),
-            Text('The Dah Great City',
-                style: TextStyle(
-                    fontSize: 15, color: Color.fromARGB(255, 170, 255, 1))),
-            SizedBox(height: 30),
-            Text('Year Founded:',
-                style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white)),
-            SizedBox(height: 10),
-            Text('2024',
-                style: TextStyle(
-                    fontSize: 15, color: Color.fromARGB(255, 170, 255, 1))),
-            SizedBox(height: 30),
-            Text('Population Size:',
-                style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white)),
-            SizedBox(height: 10),
-            Text('$population',
-                style: TextStyle(
-                    fontSize: 15, color: Color.fromARGB(255, 170, 255, 1))),
-            SizedBox(height: 30),
-            Row(
-              children: <Widget>[
-                Icon(
-                  Icons.email,
-                  color: Color.fromARGB(255, 0, 0, 0),
-                ),
-                SizedBox(width: 10),
-                Text('info@dahgreatcity.co.ke',
-                    style: TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold,
-                        color: Color.fromARGB(255, 174, 255, 1))),
-              ],
-            )
-          ],
-        ),
-      ),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Home Screen'),
+    );
+  }
+}
+
+class ServicesScreen extends StatelessWidget {
+  const ServicesScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Services Screen'),
+    );
+  }
+}
+
+class AboutScreen extends StatelessWidget {
+  const AboutScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('About Screen'),
+    );
+  }
+}
+
+class AccountScreen extends StatelessWidget {
+  const AccountScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Account Screen'),
     );
   }
 }
